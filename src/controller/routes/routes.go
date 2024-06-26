d1ab3d39f55c8a54c6a868e23831d3dc0e22b327
Everson Esteves <119083241+esteveseverson@users.noreply.github.com>
2024-06-26 17:35:22 -0300

    Add files via upload

---
+++ src/controller/routes/routes.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"crud-go/src/controller"
+
+	"github.com/gin-gonic/gin"
+)
+
+func InitRoute(r *gin.RouterGroup) {
+
+	r.GET("/getUserById/:userId", controller.FindUserById)
+	r.GET("/getUserByEmail/:userEmail", controller.FindUserByEmail)
+	r.POST("createUser", controller.CreateUser)
+	r.PUT("/updateUser/:userId", controller.updateUser)
+	r.DELETE("/deleteUser/:userId", controller.DeleteUser)
+}
